a0501113881f60436a0720a83fee59e4cbdc9541
esteban perez <esteban.perez@alteru.co>
2021-07-29 17:03:46 -0500

    eliminamos carousel

--- src/views/Homepage/index.js
+++ src/views/Homepage/index.js
@@ -1,13 +1,10 @@
 import './homepage.css'
-// import { GetData } from '../../util/getData'
-import Carousel_data from '../../components/Carousel_data'
 
 export default function Homepage(){
     
     
     return (
     <div className="home-page">
-        <Carousel_data />
         <h1>Homepage</h1>
      
        

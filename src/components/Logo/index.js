4c6de8dd48514124d3720efe5e7bf53a92f1bfb1
esteban perez <esteban.perez@alteru.co>
2021-07-29 12:52:38 -0500

    pull rama seach

--- src/components/Logo/index.js
+++
@@ -1,7 +0,0 @@
-import logo from './logo.png'
-
-export default function Logo (){
-    return <header>
-        <span><img src={logo} alt="logo"/></span>
-    </header>
-}
--- src/views/Contactenos/index.js
+++ src/views/Contactenos/index.js
@@ -1,20 +1,77 @@
-import './contactenos.css'
-import { GetData } from '../../util/getData'
+import React, { useState } from 'react';
+import './contactenos.css';
 
-export default function Contactenos(){
-     let animes = GetData()
-    return (
-    <div>
-    <div className="contacto">
-         {animes.map((anime) =>(
-            <div key={anime.id} anime={anime}>
-                <h1>{anime.title}</h1>
 
-            </div>
-        ))}
-        <h1>Contacto</h1> 
+//value={nombre} onChange={manejarNombre}
+function Contactenos() {
+   const [persona, setPersona] = useState({
+       nombre:"",
+       apellido:"",
+       correo:"",
+       telefono:"",
+       sugerencia:"",
+    });
 
-    </div>
-    </div>
-    );
+   const formulario = (event) => {
+       const {name, value} = event.target;
+       setPersona(prevPersona => ({
+           ...prevPersona, 
+           [name]: value
+       }))
+
+       console.log(persona);
+   }
+
+   //onChange(e){
+   //    this.setState({
+   //        [e.target.name]: e.target.value
+   //    })
+   //}
+
+   
+
+   const submit = (event) => {
+       event.preventDefault();
+       console.log(persona);
+   }
+
+    return(
+        <div>
+            <h2 className="H2">CONTACTENOS</h2>
+            <form className="conatiner_center" onSubmit={submit}>
+                <div >
+                   <label htmlfor="" className="label">Nombre:</label>
+                   <input  className="input" type= "text" value={persona.nombre} name="nombre" onChange={formulario}/>
+                </div>
+                <div>
+                   <label htmlfor="" className="label">Apellido:</label>
+                   <input className="input" type= "text" value={persona.apellido} name="apellido" onChange={formulario}/>
+                </div>
+                <div>
+                   <label htmlfor="" className="label">Correo:</label>
+                   <input  className="input" type= "email" value={persona.correo} name="correo" onChange={formulario}/>
+                </div>
+                <div>
+                   <label htmlfor="" className="label" > Telefono:</label>
+                   <input className="input" type= "number" value={persona.telefono} name="telefono" onChange={formulario}/>
+                </div>
+                <div className="textarea">
+                  <label htmlfor="" className="label">Comentario:</label>                   
+                  <textarea  value={persona.sugerencia} name="sugerencia" onChange={formulario}/>
+                </div>
+                <div className="button">
+                   <button >
+                       Submit
+                  </button>
+                  <span style={{color: 'green'}}></span>
+                  
+                </div>
+                        
+               
+           </form>
+        
+        </div>
+    )
 }
+
+export default Contactenos;

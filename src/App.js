92bcddb9b75199d420d96505af182e57bd0576e1
esteban perez <esteban.perez@alteru.co>
2021-07-29 12:42:13 -0500

    commit develop

--- src/App.js
+++ src/App.js
@@ -7,58 +7,32 @@ import Detail from './views/Detail';
 import Nosotros from './views/Nosotros';
 import Contactenos from './views/Contactenos';
 import Navbar from './components/Navbar';
-import React, { useState } from 'react';
-import Results from './views/Results/index';
-import SingleView from './views/SingleView/index';
-import MainNavigation from './modules/MainNavigation';
-import { SearchContext } from './util/Search/index';
 
-const App = () => {
-  const [animeData, setAnimeData] = useState([]);
-  const [singleData, setSingleData] = useState({});
 
-  const setData = (data) => {
-    setAnimeData(data);
-  };
-
-  const setSingle = (data) => {
-    setSingleData(data);
-  };
-
-  const search = (searchTerm) => {
-    return fetch(
-      `https://api.jikan.moe/v3/search/anime?q=${searchTerm}&limit=20`
-    ).then((response) => response.json());
-  };
-
-  
+function App() {
   return (
-    <SearchContext.Provider
-      value={{ search, animeData, setData, singleData, setSingle }}
-    >
-      <Router>
-        <MainNavigation />
-        <main>
-          <Switch>
-             <Route path="/" exact>
-              <Homepage />
-            </Route>
-            <Route path="/results" exact>
-              <Results />
-            </Route>
-            <Route path="/views" exact>
-              <Contactenos/>
-            </Route>
-            <Route path="/single-view" exact>
-              <SingleView />
-            </Route>
-            <Redirect to="/" />
-          </Switch>
-        </main>
-      </Router>
-    </SearchContext.Provider>
+    <div className="App">
+       <Router>
+        <Header />
+        <Switch>
+          <Route path="/detail">
+            <Detail />
+          </Route>
+          <Route path="/about">
+            <Nosotros />
+          </Route>
+          <Route path="/contactenos">
+            <Contactenos />
+          </Route>
+          
+           <Route path="/">
+            <Homepage />
+          </Route> 
+        </Switch>
+        <Footer />
+      </Router> 
+    </div>
   );
-};
+}
 
 export default App;
-

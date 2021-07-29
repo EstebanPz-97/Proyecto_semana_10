0eac202b09ec11f173ed0be38c65db867c8310e8
esteban perez <esteban.perez@alteru.co>
2021-07-29 18:27:58 -0500

    eliminamos archivos inescesarios

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Homepage from './views/Homepage';
-import Detail from './views/Detail';
-import Nosotros from './views/Nosotros';
-import Contactenos from './views/Contactenos';
+
 
 
 function App() {
   return (
     <div className="App">
-       <Router>
-        <Header />
-        <Switch>
-          <Route path="/detail">
-            <Detail />
-          </Route>
-          <Route path="/about">
-            <Nosotros />
-          </Route>
-          <Route path="/contactenos">
-            <Contactenos />
-          </Route>
-          
-           <Route path="/">
-            <Homepage />
-          </Route> 
-        </Switch>
-        <Footer />
-      </Router> 
+  
     </div>
   );
 }
--- src/views/Contactenos/index.js
+++ src/views/Contactenos/index.js
@@ -1,20 +1,10 @@
 import './contactenos.css'
-import { GetData } from '../../util/getData'
 
 export default function Contactenos(){
-     let animes = GetData()
     return (
     <div>
-    <div className="contacto">
-         {animes.map((anime) =>(
-            <div key={anime.id} anime={anime}>
-                <h1>{anime.title}</h1>
-
-            </div>
-        ))}
         <h1>Contacto</h1> 
 
-    </div>
     </div>
     );
 }

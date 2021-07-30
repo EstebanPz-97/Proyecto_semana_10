2348ffeaea868c552b83037fb9e926eb61e2a14a
Jeison Cabarcas <ancjeison@gmail.com>
2021-07-29 22:59:56 -0500

    casi sirviendo la api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import './App.css';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Homepage from './views/Homepage';
 import Nosotros from './views/Nosotros';
 import Contactenos from './views/Contactenos';
-
+import { SearchContext } from './util/Search/index';
 
 function App() {
+
+  const [animeData, setAnimeData] = useState([]);
+  const [singleData, setSingleData] = useState({});
+
+  const setData = (data) => {
+    setAnimeData(data);
+  };
+
+  const setSingle = (data) => {
+    setSingleData(data);
+  };
+
+  const search = (searchTerm) => {
+    return fetch(
+      `https://api.jikan.moe/v3/search/anime?q=${searchTerm}&limit=20`
+    ).then((response) => response.json());
+  };
+
   return (
     <div className="App">
+          <SearchContext.Provider
+      value={{ search, animeData, setData, singleData, setSingle }}
+    >
        <Router>
         <Header contactenos="contactenos" about="about" />
         <Switch>
@@ -19,12 +41,16 @@ function App() {
           <Route path="/contactenos">
             <Contactenos />
           </Route>
+            <Route path="/results" exact>
+              <Results />
+            </Route>
            <Route path="/">
             <Homepage />
           </Route> 
         </Switch>
         <Footer />
       </Router> 
+      </SearchContext.Provider>
     </div>
   );
 }

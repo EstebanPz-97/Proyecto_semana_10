d709a1637cc6807c6bee0f68945f25f3c451b6fb
Jeison Cabarcas <ancjeison@gmail.com>
2021-07-29 19:51:48 -0500

    eliminacion de archivos no usados

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Homepage from './views/Homepage';
-import Detail from './views/Detail';
 import Nosotros from './views/Nosotros';
 import Contactenos from './views/Contactenos';
 
@@ -14,9 +13,7 @@ function App() {
        <Router>
         <Header contactenos="contactenos" about="about" />
         <Switch>
-          <Route path="/detail">
-            <Detail />
-          </Route>
+
           <Route path="/about">
             <Nosotros />
           </Route>
--- src/views/Detail/index.js
+++
@@ -1,7 +0,0 @@
-import './detail.css'
-
-export default function Detail(){
-    return <div className="detail">
-        <h1>detail</h1>
-    </div>
-}

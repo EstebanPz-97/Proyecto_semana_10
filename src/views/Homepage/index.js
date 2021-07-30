526da2d9d6c40e29c138d51b864262c393a05f5e
erikablanco <84459860+erikablanco@users.noreply.github.com>
2021-07-30 11:32:03 -0500

    estilos e  imagen homepages

--- src/views/Homepage/index.js
+++ src/views/Homepage/index.js
@@ -30,13 +30,19 @@ const Homepage = () => {
     >
       <Grid item> 
         <Grid item>
-          
+        <img
+            alt="logo"
+            src={process.env.PUBLIC_URL + '/logo.png'}
+            height={200}
+            width={450}
+           
+          />
         </Grid>
         <Grid item>
           <form className="form">
             <FormControl type="submit" className="formControl">
               <Input
-                placeholder="Search for you favorite anime..."
+                placeholder="Search Anime..."
                 value={input}
                 onChange={(event) => setInput(event.target.value)}
                 className="search"
